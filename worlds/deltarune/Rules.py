30f135920caacdce244a1993c6374c0e61dae94b
Tenebrosful <tenebrosful@proton.me>
2026-07-30 09:39:15 +0200

    Change Jigsawry and Ponman recruitment logic

--- worlds/deltarune/Rules.py
+++ worlds/deltarune/Rules.py
@@ -50,8 +50,12 @@ can_act_spare_ralsei = have_ralsei & have_actions
 ### CHAPTER 1 RECRUITMENT
 can_recruit_ruddin = have_kris | can_recruit_with_violence_ralsei
 can_recruit_hathy = have_kris | can_recruit_with_violence_ralsei
-can_recruit_jigsawry = have_kris | can_recruit_with_violence_ralsei
-can_recruit_ponman = have_kris | can_recruit_with_violence_ralsei
+can_recruit_jigsawry = have_kris_and_ralsei | (
+    CanReachRegion(Regions.ch1_card_castle) & (have_kris | can_recruit_with_violence_ralsei)
+)
+can_recruit_ponman = have_kris_and_ralsei | (
+    CanReachRegion(Regions.ch1_card_castle) & (have_kris | can_recruit_with_violence_ralsei)
+)
 can_recruit_rabbick = have_kris | can_recruit_with_violence_ralsei
 can_recruit_bloxer = have_kris | can_recruit_with_violence_ralsei
 can_recruit_head_hathy = have_kris | can_recruit_with_violence_ralsei
